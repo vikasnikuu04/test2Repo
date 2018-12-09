f106dfce4a6f8906959b421a7e91871a29abf946
vikasnikuu04 <45723524+vikasnikuu04@users.noreply.github.com>
2018-12-09 03:53:11 +0000

    Update TestTest.java

--- src/test/java/test2Repo/TestTest.java
+++ src/test/java/test2Repo/TestTest.java
@@ -1,7 +1,7 @@
 package test2Repo;
 
 public class TestTest {
-	public void test1conflict( ) {
+	public void test1conflict11( ) {
 		System.out.println("test1conflict");
 		System.out.println("test1conflict");
  
